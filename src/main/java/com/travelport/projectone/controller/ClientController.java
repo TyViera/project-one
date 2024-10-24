976909961a1514b8bce498f6bceae3785c6b4b96
Anastasiia Levkina <anastasiia.levkina.7e5@itb.cat>
2024-10-24 10:13:22 +0200

    ClientService + ClientController

--- src/main/java/com/travelport/projectone/controller/ClientController.java
+++ src/main/java/com/travelport/projectone/controller/ClientController.java
@@ -1,4 +1,43 @@
 package com.travelport.projectone.controller;
 
+import com.travelport.projectone.entities.Client;
+import com.travelport.projectone.service.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.image.RescaleOp;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) { this.clientService = clientService; }
+
+    @GetMapping
+    public List<Client> getClients() {
+        return clientService.getClients();
+    }
+
+    @PostMapping
+    public Client postClient(@RequestBody Client client) { return clientService.save(client); }
+
+    @GetMapping("/{nif}")
+    public ResponseEntity<Client> getClientByNif(@PathVariable("nif") String nif) {
+        var client = clientService.findByNif(nif);
+        return client.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{nif}")
+    public ResponseEntity<Client> updateClient(@PathVariable("nif") String nif, @RequestBody Client client) {
+        return ResponseEntity.ok(clientService.update(nif, client));
+    }
+
+    @DeleteMapping("/{nif}")
+    public ResponseEntity<Client> deleteClient(@PathVariable("nif") String nif) {
+        clientService.deleteByNif(nif);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/travelport/projectone/service/ClientService.java
+++ src/main/java/com/travelport/projectone/service/ClientService.java
@@ -1,5 +1,19 @@
 package com.travelport.projectone.service;
 
+import com.travelport.projectone.entities.Client;
+import java.util.List;
+import java.util.Optional;
+
+
 public interface ClientService {
 
+    List<Client> getClients();
+
+    Optional<Client> findByNif(String nif);
+
+    void deleteByNif(String nif);
+
+    Client save(Client client);
+
+    Client update(String nif, Client client);
 }
