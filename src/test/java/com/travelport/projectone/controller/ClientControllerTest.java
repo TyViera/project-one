54194ec0af95fd69add1006f4668dff91cc45f81
Anastasiia Levkina <anastasiia.levkina.7e5@itb.cat>
2024-10-24 11:38:51 +0200

    ClientDaoImplTest

---
+++ src/test/java/com/travelport/projectone/controller/ClientControllerTest.java
@@ -0,0 +1,60 @@
+package com.travelport.projectone.controller;
+
+import com.travelport.projectone.config.DatabaseConfig;
+import com.travelport.projectone.config.JpaRepositoryConfig;
+import com.travelport.projectone.config.WebAppConfig;
+import com.travelport.projectone.config.WebAppInitializer;
+import com.travelport.projectone.persistence.iml.ClientDaoImpl;
+import jakarta.persistence.Entity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.sql.DataSource;
+
+@WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        WebAppConfig.class,
+        WebAppInitializer.class,
+        JpaRepositoryConfig.class,
+        DatabaseConfig.class,
+        ClientController.class,
+        ClientDaoImpl.class
+})
+class ClientControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private DataSource dataSource;
+
+    MockMvc mockMvc;
+
+    @Test
+    void getClientsTest() {
+    }
+
+    @Test
+    void postClient() {
+    }
+
+    @Test
+    void getClientByNif() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+}
