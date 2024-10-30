2bf9025127c32ef5e16d41490c07ac8c2a1c16ac
Anastasiia Levkina <anastasiia.levkina.7e5@itb.cat>
2024-10-30 10:30:59 +0100

    Va tomcat (sin JPA). Conexión a Postgres (no va).

--- src/main/java/com/travelport/projectone/jpa/ProductJpaRepository.java
+++ src/main/java/com/travelport/projectone/jpa/ProductJpaRepository.java
@@ -3,4 +3,4 @@ package com.travelport.projectone.jpa;
 import com.travelport.projectone.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductJpaRepository extends JpaRepository<Product, Integer> {}
+public interface ProductJpaRepository extends JpaRepository<Product, Integer> { }
--- src/main/java/com/travelport/projectone/jpa/PurchaseJpaRepository.java
+++ src/main/java/com/travelport/projectone/jpa/PurchaseJpaRepository.java
@@ -3,4 +3,4 @@ package com.travelport.projectone.jpa;
 import com.travelport.projectone.entities.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PurchaseJpaRepository extends JpaRepository<Purchase, Integer> {}
+public interface PurchaseJpaRepository extends JpaRepository<Purchase, Integer> { }
