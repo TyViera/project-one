cd1250982196f57dc791fa2b1c86fe7f5c667303
annacano0 <anna.elisabet5@gmail.com>
2024-11-03 18:39:52 +0100

    test: add unit testing to services

---
+++ src/test/java/com/travelport/projectone/service/ClientServiceImplTest.java
@@ -0,0 +1,161 @@
+package com.travelport.projectone.service;
+
+import com.travelport.projectone.entities.Client;
+import com.travelport.projectone.persistence.ClientDao;
+import com.travelport.projectone.service.impl.ClientServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientServiceImplTest {
+    @InjectMocks
+    private ClientServiceImpl clientService;
+
+    @Mock
+    private ClientDao clientDao;
+
+    @Test
+    void findAllClients(){
+        //Set up - given
+        Client client1 = new Client();
+        client1.setName("Pepito Doe");
+        client1.setAddress("Carrer Aiguablava");
+        client1.setNif("123123123Z");
+
+        Client client2 = new Client();
+        client2.setName("Manuela Doe");
+        client2.setAddress("Carrer Aiguablava 34");
+        client2.setNif("123134123S");
+
+
+        List<Client> clients = new ArrayList<>();
+        clients.add(client1);
+        clients.add(client2);
+        when(clientDao.findAll()).thenReturn(clients);
+
+        //Do something - when
+        List<Client> result = clientService.findAll();
+
+        //Asserts -Then
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(clients, result);
+        verify(clientDao, times(1)).findAll();
+    }
+
+    @Test
+    void findClientById_ClientExists() {
+        // Set up - given
+        int clientId = 1;
+        Client client = new Client();
+        client.setId(clientId);
+        client.setName("Pepito Doe");
+
+        when(clientDao.findById(clientId)).thenReturn(Optional.of(client));
+
+        // Do something - when
+        Optional<Client> result = clientService.findById(clientId);
+
+        // Asserts - Then
+        assertTrue(result.isPresent());
+        assertEquals(client, result.get());
+        verify(clientDao, times(1)).findById(clientId);
+    }
+
+    @Test
+    void findClientById_ClientDoesNotExist() {
+        // Set up - given
+        int clientId = 1;
+        when(clientDao.findById(clientId)).thenReturn(Optional.empty());
+
+        // Do something - when
+        Optional<Client> result = clientService.findById(clientId);
+
+        // Assert - Then
+        assertFalse(result.isPresent());
+        verify(clientDao, times(1)).findById(clientId);
+    }
+
+    @Test
+    void deleteClientById() {
+        // Set up - given
+        int clientId = 1;
+
+        // Do something - when
+        clientService.deleteById(clientId);
+
+        // Assert - Then
+        verify(clientDao, times(1)).deleteById(clientId);
+    }
+
+    @Test
+    void saveClient() {
+        // Set up - given
+        Client client = new Client();
+        client.setName("Pepito Doe");
+
+        when(clientDao.save(client)).thenReturn(client);
+
+        // Do something - when
+        Client result = clientService.save(client);
+
+        // Asserts - Then
+        assertNotNull(result);
+        assertEquals(client, result);
+        verify(clientDao, times(1)).save(client);
+    }
+
+    @Test
+    void updateClient_ClientExists() {
+        // Set up - given
+        int clientId = 1;
+        Client clientToUpdate = new Client();
+        clientToUpdate.setName("Updated Name");
+        clientToUpdate.setAddress("Updated Address");
+
+        Client existingClient = new Client();
+        existingClient.setId(clientId);
+        existingClient.setName("Original Name");
+        existingClient.setAddress("Original Address");
+
+        when(clientDao.findById(clientId)).thenReturn(Optional.of(existingClient));
+
+        // Do something - when
+        Client result = clientService.update(clientId, clientToUpdate);
+
+        // Asserts - Then
+        assertNotNull(result);
+        assertEquals(clientToUpdate.getName(), result.getName());
+        assertEquals(clientToUpdate.getAddress(), result.getAddress());
+        verify(clientDao, times(1)).findById(clientId);
+        verify(clientDao, times(1)).update(existingClient);
+    }
+
+    @Test
+    void updateClient_ClientDoesNotExist() {
+        // Set up - given
+        int clientId = 1;
+        Client clientToUpdate = new Client();
+        clientToUpdate.setName("Updated Name");
+
+        when(clientDao.findById(clientId)).thenReturn(Optional.empty());
+
+        // Do something - when
+        Client result = clientService.update(clientId, clientToUpdate);
+
+        // Asserts - Then
+        assertNull(result);
+        verify(clientDao, times(1)).findById(clientId);
+        verify(clientDao, times(0)).update(any(Client.class));
+    }
+
+}
---
+++ src/test/java/com/travelport/projectone/service/SaleServiceImplTest.java
@@ -0,0 +1,167 @@
+package com.travelport.projectone.service;
+
+import com.travelport.projectone.dto.*;
+import com.travelport.projectone.entities.*;
+import com.travelport.projectone.persistence.*;
+import com.travelport.projectone.service.impl.SaleServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SaleServiceImplTest {
+
+    @InjectMocks
+    private SaleServiceImpl saleService;
+
+    @Mock
+    private SaleDao saleDao;
+
+    @Mock
+    private ProductDao productDao;
+
+    @Mock
+    private ClientDao clientDao;
+
+    @Test
+    void saveSale_ValidRequest() {
+        // Set up - given
+        SaleRequest saleRequest = new SaleRequest();
+        saleRequest.setClientId(1);
+
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setId(1);
+        productRequest.setQuantity(2);
+        saleRequest.setProducts(List.of(productRequest));
+
+        Client client = new Client();
+        client.setId(1);
+
+        Product product = new Product();
+        product.setId(1);
+
+        Sale sale = new Sale();
+        sale.setId(1);
+        sale.setClient(client);
+        sale.setSellDate(new Timestamp(System.currentTimeMillis()));
+
+        when(clientDao.findById(1)).thenReturn(Optional.of(client));
+        when(productDao.findById(1)).thenReturn(Optional.of(product));
+        when(saleDao.saveSale(any(Sale.class))).thenReturn(sale);
+
+        // Do something - when
+        Sale result = saleService.saveSale(saleRequest);
+
+        // Asserts - then
+        assertNotNull(result);
+        assertEquals(client, result.getClient());
+        verify(saleDao, times(1)).saveSale(any(Sale.class));
+        verify(saleDao, times(1)).saveSaleDetail(any(SaleDetail.class));
+    }
+
+    @Test
+    void saveSale_InvalidProductQuantity() {
+        // Set up - given
+        SaleRequest saleRequest = mock(SaleRequest.class);
+        when(saleRequest.areProductsQuantityValid()).thenReturn(false);
+
+        // Do somwthing / Assert
+        assertThrows(IllegalArgumentException.class, () -> saleService.saveSale(saleRequest));
+    }
+
+    @Test
+    void saveSale_ClientNotFound() {
+        // Set up - given
+        SaleRequest saleRequest = new SaleRequest();
+        saleRequest.setClientId(1);
+
+        when(clientDao.findById(1)).thenReturn(Optional.empty());
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> saleService.saveSale(saleRequest));
+    }
+
+    @Test
+    void saveSale_ProductNotFound() {
+        // Set up - given
+        SaleRequest saleRequest = new SaleRequest();
+        saleRequest.setClientId(1);
+
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setId(1);
+        productRequest.setQuantity(2);
+        saleRequest.setProducts(List.of(productRequest));
+
+        Client client = new Client();
+        client.setId(1);
+
+        when(clientDao.findById(1)).thenReturn(Optional.of(client));
+        when(productDao.findById(1)).thenReturn(Optional.empty());
+
+        // Assert - Then
+        assertThrows(IllegalArgumentException.class, () -> saleService.saveSale(saleRequest));
+    }
+
+    @Test
+    void findAllSales() {
+        // Set up - given
+        int clientId = 1;
+        Sale sale = new Sale();
+        sale.setId(1);
+
+        SaleDetail saleDetail = new SaleDetail();
+        saleDetail.setProductId(1);
+        saleDetail.setQuantity(2);
+
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Product 1");
+        product.setCode("ASFE1");
+
+        // Do something - when
+        when(saleDao.findAll(clientId)).thenReturn(List.of(sale));
+        when(saleDao.findSaleDetailsBySaleId(sale.getId())).thenReturn(List.of(saleDetail));
+        when(productDao.findById(saleDetail.getProductId())).thenReturn(Optional.of(product));
+
+        List<SaleResponse> result = saleService.findAll(clientId);
+
+        // Asserts - then
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(sale.getId(), result.get(0).getSaleId());
+        verify(saleDao, times(1)).findAll(clientId);
+        verify(saleDao, times(1)).findSaleDetailsBySaleId(sale.getId());
+    }
+
+    @Test
+    void getIncomeReport() {
+        // Set up - given
+        ReportResponse reportResponse = new ReportResponse();
+        reportResponse.setId(1);
+
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Product 1");
+
+        when(saleDao.getIncomeReport()).thenReturn(List.of(reportResponse));
+        when(productDao.findById(reportResponse.getId())).thenReturn(Optional.of(product));
+
+        // Do something - when
+        List<ReportResponse> result = saleService.getIncomeReport();
+
+        // Asserts - then
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(product.getName(), result.get(0).getName());
+        verify(saleDao, times(1)).getIncomeReport();
+        verify(productDao, times(1)).findById(reportResponse.getId());
+    }
+}
