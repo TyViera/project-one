0a374d3cb9738e4c2f0b9eda05e7f63b69afd727
eeriic23 <erichega05@gmail.com>
2024-11-07 12:10:59 +0100

    Remove unused Jpa classes

--- src/main/java/com/travelport/jpa/ProductJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travelport.jpa;
-
-import com.travelport.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductJpaRepository extends JpaRepository<Product, Integer> {}
