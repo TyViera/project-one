8bd4442dc797428441e178f259b999d8e8a2a1c5
annacano0 <anna.elisabet5@gmail.com>
2024-11-10 18:24:52 +0100

    refactor: remove unused jpa repositories

--- src/main/java/com/travelport/projectone/jpa/ClientJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.travelport.projectone.jpa;
-
-import com.travelport.projectone.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientJpaRepository extends JpaRepository<Client, Integer> {}
